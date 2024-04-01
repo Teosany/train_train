21706d7c3bda0980bd37f7b57acc2b10a692ccab
Teo <teosany@icloud.com>
2024-04-01 14:03:54 +0200

    add Convert number to reversed array of digits

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,29 @@ fn main () {
 
 }
 
+//Convert number to reversed array of digits
+// fn digitize(n: u64) -> Vec<u8> {
+//     let mut digits= Vec::new();
+//     let mut n= n;
+//     while n > 9 {
+//         digits.push((n % 10) as u8);
+//         n = n / 10;
+//     }
+//     digits.push(n as u8);
+//     digits
+// }
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//
+//     #[test]
+//     fn test_fixed() {
+//         assert_eq!(digitize(348597), vec![7,9,5,8,4,3]);
+//         assert_eq!(digitize(35231), vec![1,3,2,5,3]);
+//         assert_eq!(digitize(0), vec![0]);
+//     }
+// }
+
 //Reversed sequence
 
 // fn reverse_seq(n: u32) -> Vec<u32> {
