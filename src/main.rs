d2df8f2dbf24c01514688f7f7d3eccada18f25a8
Teo <teosany@icloud.com>
2024-04-01 13:28:01 +0200

    first

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
 fn main () {
+    let mut s = String::from("he llo world");
 
+    let world = premier_mot(&s);
+
+    // s.clear();
+
+    println!("{world}");
+
+    s.clear();
+    // println!("{s}");
+    println!("{world}");
+    // println!("{s}");
+
+    // let s = String::from("hello world");
+
+    // let hello = &s[..5];
+    // let hello = &s[0..5];
+    // let world = &s[6..11];
+    // let world = &s[6..];
+    // let world = &s[..];
+    // println!("{world}")
 }
+
+// fn second_mot(s: &String) -> &str {
+//
+// }
+
+fn premier_mot(s: &String) -> &str {
+    let octets = s.as_bytes();
+
+    for (i, &element) in octets.iter().enumerate() {
+        if element == b' ' {
+            return &s[..i];
+        }
+    }
+    &s[..]
+}
+
+// fn premier_mot(s: &String) -> usize {
+//     let octets = s.as_bytes();
+//     println!("{:?}", octets);
+//
+//     for (i, &element) in octets.iter().enumerate() {
+//         println!("{i}, {element}");
+//
+//         if element == b' ' {
+//             return i;
+//         }
+//     }
+//
+//     s.len()
+// }
+
+//soit en slice
